bb2b69e9c8b39c5ac87976f00cced85cb9dc02aa
Mukul <mukulxt@gmail.com>
2024-12-05 16:03:29 +0530

    done

--- src/api.ts
+++ src/api.ts
@@ -1,2 +1,2 @@
-export const API = "https://architex-staging.designelementary.com/api";
+export const API = "https://architex.designelementary.com/api";
 export const DASHBOARD_URL = "https://b2c.designelementary.in/#"
